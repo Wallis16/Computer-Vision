5c4ca6ea0905684693353de7e81ab060bd0c7df9
Diógenes Wallis de França Silva <diogeneswallis@outlook.com>
2019-07-03 21:53:54 -0300

    Add files via upload

--- facerec_from_webcam_faster.py
+++ facerec_from_webcam_faster.py
@@ -71,7 +71,11 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
-frame_IN = np.zeros((480,640,3),np.uint8)
+ret, frame = video_capture.read()
+frame = cv2.resize(frame, (0, 0), fx=1.4, fy=1.4)
+
+
+frame_IN = np.zeros((frame.shape[0],frame.shape[1],3),np.uint8)
 
 while True:
     # Grab a single frame of video
@@ -79,13 +83,14 @@ while True:
     
 	flag_detection = False
 	time_detection_face_diogenes = 0
-
+	#frame = cv2.resize(frame, (0, 0), fx=1.2, fy=1.2)
     # Resize frame of video to 1/4 size for faster face recognition processing
 	small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
 	rgb_small_frame = small_frame[:, :, ::-1]
 
+	frame = cv2.resize(frame, (0, 0), fx=1.4, fy=1.4)
     # Only process every other frame of video to save time
 	if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
@@ -96,7 +101,7 @@ while True:
 		for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
 			matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-			name = "Nao identificado"
+			name = "Desconhecido"
 
             # # If a match was found in known_face_encodings, just use the first one.
             # if True in matches:
@@ -120,19 +125,20 @@ while True:
 		flag_detection = True
 
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-		top *= 4
-		right *= 4
-		bottom *= 4
-		left *= 4
+		top *= 5.6
+		right *= 5.6
+		bottom *= 5.6
+		left *= 5.6
+		top, right, bottom, left = int(top), int(right), int(bottom), int(left)
 
-		if right < 310:
+		if right < (int(frame.shape[1]/2)-10):
             # Draw a box around the face
 			cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
         
             # Draw a label with a name below the face
 			cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         
-		if left > 330:
+		if left > (int(frame.shape[1]/2)+10):
             # Draw a box around the face
 			cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 255), 2)
         
@@ -146,9 +152,9 @@ while True:
 
 		cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-		if right < 310:
+		if right < (int(frame.shape[1]/2)-10):
 
-			if name == "Diogenes" and flag_IN == True:
+			if name != "Desconhecido" and flag_IN == True:
                 
 				try:
 					if reference == 0:
@@ -158,16 +164,17 @@ while True:
 					if reference != 0: 
 						time_detection_face_diogenes = time.time() - reference_ct
                 	            
-						if time.time() - reference_ct > 2:
-							string_IN.append("Diogenes - " + str(reference) + "\n")
+						if time.time() - reference_ct > 1:
+							string_IN.append(name + " - " + str(reference) + "\n")
 							flag_OUT = True
 							flag_IN = False       
+							print(string_IN)
 				except:
 					reference=0
 
-		if left > 330:
+		if left > (int(frame.shape[1]/2)+10):
 
-			if name == "Diogenes" and flag_OUT == True:
+			if name != "Desconhecido" and flag_OUT == True:
                 
 				try:
 					if reference == 0:
@@ -177,14 +184,14 @@ while True:
 					if reference != 0: 
 						time_detection_face_diogenes = time.time() - reference_ct
                             
-						if time.time() - reference_ct > 2:
-							string_OUT.append("Diogenes - " + str(reference) + "\n")
+						if time.time() - reference_ct > 1:
+							string_OUT.append(name + " - " + str(reference) + "\n")
 							flag_OUT = False
 							flag_IN = True                                  
 				except:
 					reference=0
 
-		if right > 310 and left < 330:
+		if right > (int(frame.shape[1]/2)-10) and left < (int(frame.shape[1]/2)+10):
 			reference = 0
 
 
@@ -198,10 +205,10 @@ while True:
     
 	cv2.putText(frame, current_time, (0,20), font, 0.5, (255, 0, 0), 1)
 	cv2.putText(frame, "Entry", (0,50), cv2.FONT_HERSHEY_TRIPLEX, 0.75, (255, 0, 0), 1)
-	cv2.putText(frame, "Exit", (340,50), cv2.FONT_HERSHEY_TRIPLEX, 0.75, (255, 0, 0), 1)
-    
-	frame_IN[0:480,0:310] = frame[0:480,0:310]
-	frame_IN[0:480,330:640] = frame[0:480,330:640]
+	cv2.putText(frame, "Exit", (int(frame.shape[1]/2)+15,50), cv2.FONT_HERSHEY_TRIPLEX, 0.75, (255, 0, 0), 1)
+	#print(frame.shape) 
+	frame_IN[0:frame.shape[0],0:(int(frame.shape[1]/2)-10)] = frame[0:frame.shape[0],0:int(frame.shape[1]/2-10)]
+	frame_IN[0:frame.shape[0],(int(frame.shape[1]/2)+10):frame.shape[1]] = frame[0:frame.shape[0],int(frame.shape[1]/2+10):frame.shape[1]]
     
     # Display the resulting image
 	cv2.imshow('Video', frame_IN)
@@ -227,13 +234,38 @@ for line in string_OUT:
 	outF.write(line)
 outF.close()
 
-num_lines = 0
+"""num_lines = 0
 with open("FACE_OUT.txt", 'r') as f:
 	for line in f:
 		num_lines += 1
 
 file = open("FACE_NUMB.txt","w")
 file.write("Diogenes - " + str(num_lines) + "\n")
-file.close()
+file.close()"""
+
+nomes = []
+
+with open("FACE_OUT.txt", 'r') as f:
+    for line in f:
+        aux = line.split()
+        nomes.append(aux[0])
+
+cont = 0
+cont_numb = []
+
+for string in nomes:
+    for i in range(len(nomes)):
+        if string == nomes[i]:
+            cont += 1
+    cont_numb.append(string + " - " + str(cont) + "\n")
+    cont = 0
+
+outF = open("FACE_NUMB.txt", "w")
+for line in cont_numb:
+  # write line to output file
+	outF.write(line)
+outF.close()
+
+
 video_capture.release()
 cv2.destroyAllWindows()
